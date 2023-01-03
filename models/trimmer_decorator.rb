7ef298c43798eb196ee62cc51271fbf0757061b2
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2023-01-03 18:24:18 +0300

    fix code

--- models/trimmer_decorator.rb
+++ models/trimmer_decorator.rb
@@ -2,7 +2,7 @@ require_relative 'base_decorator'
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    return @nameable.correct_name[0..9] unless super.length <= 10
+    return @nameable_obj.correct_name[0..9] unless super.length <= 10
 
     super
   end
