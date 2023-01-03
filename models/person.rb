89be46f67a385bf8acda131a5d81d120c3b57d72
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2023-01-03 14:03:50 +0300

    add person, student and teacher classes

---
+++ models/person.rb
@@ -0,0 +1,26 @@
+class Person
+  #getters & setters
+  attr_reader :id
+  attr_accessor :name, :age
+
+  #constructor
+  def initialize(age, name = "Unknown", parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  #private method
+  def is_of_age?
+    @age >= 18
+  end
+  private :is_of_age?
+
+  #public method
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
+
+end
